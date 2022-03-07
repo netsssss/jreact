e1966e0717c464a116abcd48630241dd0c067032
netsssss <1543482826@qq.com>
2022-03-07 14:36:21 +0800

    feat: ts

---
+++ src/utils/index.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+  if (!object) return {};
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+// hooks
+export const useDebounce = <T>(value: T, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
